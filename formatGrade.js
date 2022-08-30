0cd16abca89924e14d13f4dbbc4cf1aa305b12bf
Bozhidar Genchev <107246087+kolevgenchev@users.noreply.github.com>
2022-08-30 20:13:53 +0300

    Add files via upload

---
+++ formatGrade.js
@@ -0,0 +1,16 @@
+function format(grade){
+
+    if(grade>=2&&grade<3){
+        console.log(`Fail (2)`);
+    }else if(grade>=3&&grade<3.50){
+        console.log(`Poor (${grade.toFixed(2)})`);
+    }else if(grade>=3.50&&grade<4.50){
+        console.log(`Good (${grade.toFixed(2)})`);
+    }else if(grade>=4.50&&grade<5.50){
+        console.log(`Very good (${grade.toFixed(2)})`);    
+    }else if(grade>=5.50&&grade<6.01){
+        console.log(`Excellent (${grade.toFixed(2)})`);
+    }
+
+}
+format(5.50)
