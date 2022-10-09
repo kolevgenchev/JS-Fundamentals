3d87e2ea45a2de139ceef80dca73bd7ad35264b7
Bozhidar Genchev <107246087+kolevgenchev@users.noreply.github.com>
2022-10-09 12:29:21 +0300

    Add files via upload

---
+++ addAndSubtract3.js
@@ -0,0 +1,11 @@
+function addAndSub(num1,num2,num3){
+    function sum (a,b){
+      return a+b;
+    }
+    function subtract(a,b){
+        return a-b
+    }
+
+let subtractt=subtract(sum(num1,num2),num3)
+console.log(subtractt)
+}addAndSub(23,6,10)
