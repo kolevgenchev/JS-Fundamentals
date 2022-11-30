9e613b260a19d8d671e31d8eca8bda8c26a0cbdf
Bozhidar Genchev <107246087+kolevgenchev@users.noreply.github.com>
2022-11-30 20:51:38 +0200

    Add files via upload

---
+++ matchFullName.js
@@ -0,0 +1,7 @@
+function matchNewName(str){
+
+let pattern=/\b[A-Z][a-z]+ [A-Z][a-z]+\b/g
+let result=str.match(pattern)
+console.log(result.join(" "))
+
+}matchNewName("Ivan Ivanov, Ivan ivanov, ivan Ivanov, IVan Ivanov, Test Testov, Ivan	Ivanov")
